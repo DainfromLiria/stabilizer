a64286dc20a8346c688b381dfa3d933acd9fe7db
DainfromLiria <mayor3217@gmail.com>
2024-07-08 21:05:18 +0200

    Decrease BUFFER_SIZE. Add helper settings for error elimination.

--- stabilizer/settings.py
+++ stabilizer/settings.py
@@ -1,8 +1,11 @@
 """
     Main project settings.
 """
-BUFFER_SIZE = 30
+BUFFER_SIZE = 10
 FEATURE_PARAMS = dict(maxCorners=200, qualityLevel=0.01, minDistance=30, blockSize=3)
 TEXT_PARAMS = dict(org=(10, 30), fontFace=0, fontScale=1, color=(0, 255, 0), thickness=2)
 CROP_MARGIN = 0.1  # float number from 0 to 1
 PREVIEW_RESOLUTION = (700, 700)
+MIN_DISTANCE = -100
+MAX_DISTANCE = 100
+BP_THRESHOLD = 0.5
