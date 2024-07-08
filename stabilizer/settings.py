61338e7e20b8b6cc797b65b93d339fd1b96ea218
DainfromLiria <mayor3217@gmail.com>
2024-07-08 13:08:25 +0200

    Final version?

--- stabilizer/settings.py
+++ stabilizer/settings.py
@@ -4,5 +4,5 @@
 BUFFER_SIZE = 30
 FEATURE_PARAMS = dict(maxCorners=200, qualityLevel=0.01, minDistance=30, blockSize=3)
 TEXT_PARAMS = dict(org=(10, 30), fontFace=0, fontScale=1, color=(0, 255, 0), thickness=2)
-CROP_MARGIN = 0.1
+CROP_MARGIN = 0.1  # float number from 0 to 1
 PREVIEW_RESOLUTION = (700, 700)
--- stabilizer/stabilizer.py
+++ stabilizer/stabilizer.py
@@ -52,34 +52,58 @@ class Stabilizer:
             video_queue = device.getOutputQueue(name="video")
             while True:
                 frame = self.receive_frame(video_queue)
-                stab_frame = self.stabilize_frames(frame)
+                stab_frame = self.stabilize_frame(frame)
                 self.show_result(frame, stab_frame)
                 # Exit on esc or q
                 if cv2.waitKey(1) in (ord('q'), 27):
                     break
             cv2.destroyAllWindows()
 
-    def stabilize_frames(self, frame: np.ndarray) -> np.ndarray:
+    def stabilize_frame(self, frame: np.ndarray) -> np.ndarray:
         self.estimate_motion(frame)
-        return self.smooth_transformation(frame)
+        return self.smooth_and_warp(frame)
 
-    def estimate_motion(self, frame: np.ndarray):
+    def estimate_motion(self, frame: np.ndarray) -> None:
         """
-            Detect feature points in the previous and current frame.
+            Detect feature points in the current frame. Using this points,
+            calculate optical flow between current and previous frames (find this points
+            in the previous frame). Using points from previous frames and current frame
+            find homography 3x3 rotation matrix. Compute accumulate rotation matrix for this frame
+            and add it on the end of the buffer.
         """
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        prev_pts = cv2.goodFeaturesToTrack(gray, **FEATURE_PARAMS)
-        if prev_pts is not None:
-            curr_pts, _, _ = cv2.calcOpticalFlowPyrLK(self.__prev_frame, gray, prev_pts, nextPts=None)
-            if curr_pts.shape[0] >= 4 and prev_pts.shape[0] >= 4:
-                h, _ = cv2.findHomography(prev_pts, curr_pts, cv2.RANSAC, 5.0)
+        curr_pts = cv2.goodFeaturesToTrack(gray, **FEATURE_PARAMS)
+        if curr_pts is not None:
+            prev_pts, _, _ = cv2.calcOpticalFlowPyrLK(self.__prev_frame, gray, curr_pts, nextPts=None)
+            if prev_pts.shape[0] >= 4 and prev_pts.shape[0] >= 4:
+                # x, y = (prev_pts - curr_pts)[0][0]
+                # if x >= 100 or y >= 100 or x <= -100 or y <= -100:
+                #     print((prev_pts - curr_pts)[0][0])
+                h, _ = cv2.findHomography(curr_pts, prev_pts, cv2.RANSAC, 5.0)
                 if h is not None:
-                    self.__buffer.append(np.dot(h, self.__buffer[-1]))  # accumulate transformation matrix
-        if len(self.__buffer) == BUFFER_SIZE:
+                    self.__buffer.append(np.dot(h, self.__buffer[-1]))  # accumulate rotation matrix
+
+        if len(self.__buffer) > BUFFER_SIZE:
             self.__buffer.popleft()
+            # normalize buffer matrices by last matrix to
+            # prevent errors after low or high frequency big shakes
+            inv = lin.inv(self.__buffer[0])
+            for i in range(len(self.__buffer)):
+                self.__buffer[i] = np.dot(self.__buffer[i], inv)
+
         self.__prev_frame = gray
 
-    def smooth_transformation(self, frame: np.ndarray) -> np.ndarray:
+    def smooth_and_warp(self, frame: np.ndarray) -> np.ndarray:
+        """
+            Calculate average rotation matrix through all matrices in buffer and normalise it.
+            Using this matrix, warp input image and crop it. In case if original matrix
+            is singular (inversion does not exist) returns cropped version of original image.
+
+            Parameter:
+                frame - np.ndarray - original image.
+            Return:
+                np.ndarray - stabilized version of original image.
+        """
         try:
             s_h = np.dot(np.average(np.array(self.__buffer), 0), lin.inv(self.__buffer[-1]))
         except lin.LinAlgError:
@@ -88,7 +112,9 @@ class Stabilizer:
         return self.crop_frame(s_frame)
 
     def crop_frame(self, frame: np.ndarray) -> np.ndarray:
-        # Calculate the cropping margins (10% of the width and height)
+        """
+            Crop input frame on CROP_MARGIN percent, which is defined in settings.py
+        """
         x_start = int(CROP_MARGIN * self.__w)
         y_start = int(CROP_MARGIN * self.__h)
         x_end = self.__w - x_start
@@ -96,10 +122,12 @@ class Stabilizer:
         return frame[y_start:y_end, x_start:x_end]
 
     def show_result(self, orig_img: np.ndarray, stab_img: np.ndarray) -> None:
+        """
+            Show original image and stabilized image if preview mode is enabled.
+            Otherwise, show only stabilized image.
+        """
         if self.__preview_mode is True:
             cv2.putText(orig_img, 'Original', **TEXT_PARAMS)
             cv2.putText(stab_img, 'Stabilized', **TEXT_PARAMS)
             cv2.imshow("original", orig_img)
-            cv2.imshow("stabilized", stab_img)
-        else:
-            cv2.imshow("stabilized", stab_img)
+        cv2.imshow("stabilized", stab_img)
