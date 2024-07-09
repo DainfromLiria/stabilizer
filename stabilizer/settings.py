177d268e8d708a58b55707e4a32ae76ab57ff318
DainfromLiria <mayor3217@gmail.com>
2024-07-09 12:10:24 +0200

    Decrease BUFFER_SIZE and add threshold for preview mode.

--- stabilizer/settings.py
+++ stabilizer/settings.py
@@ -1,11 +1,12 @@
 """
     Main project settings.
 """
-BUFFER_SIZE = 10
+BUFFER_SIZE = 5
 FEATURE_PARAMS = dict(maxCorners=200, qualityLevel=0.01, minDistance=30, blockSize=3)
 TEXT_PARAMS = dict(org=(10, 30), fontFace=0, fontScale=1, color=(0, 255, 0), thickness=2)
 CROP_MARGIN = 0.1  # float number from 0 to 1
 PREVIEW_RESOLUTION = (700, 700)
 MIN_DISTANCE = -100
 MAX_DISTANCE = 100
-BP_THRESHOLD = 0.5
+BP_THRESHOLD = 2
+BP_THRESHOLD_VIEW = 0.5
